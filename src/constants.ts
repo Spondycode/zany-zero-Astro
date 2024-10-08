8433b18f5450b08141202b947e30dd1ddaccc4a3
David Allen <spondicious@me.com>
2024-10-08 04:03:01 +0200

    Looking good so far

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+
+export const SITE_TITLE = 'Tech by Spondycode';
+export const SITE_DESCRIPTION = 'Articles, Stories and stuff like that!';
+export const HOME_PAGE_ARTICLE_LIMIT = 6;
+export const ARTICLE_PER_PAGE = 6;
